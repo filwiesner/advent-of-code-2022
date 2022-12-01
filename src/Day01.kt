d7b64b6e11ed2c0e3c60abbfabb5e9355ca338df
Filip Wiesner <filip.wiesner@matee.cz>
2022-12-01 23:05:22 +0100

    Day1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+fun getElfCalories(input: List<String>) = buildList {
+    var sum = 0
+    for (line in input) {
+        if (line.isBlank()) {
+            add(sum)
+            sum = 0
+        } else sum += line.toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    add(sum)
+}
+
+fun main() {
+    fun part1(input: List<String>): Int =
+        getElfCalories(input).max()
+
+    fun part2(input: List<String>): Int =
+        getElfCalories(input)
+            .sortedDescending()
+            .take(3)
+            .sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
